933365f54f76f28ebd46a998d292a42d316e2035
Ivan Castillo <total180training@gmail.com>
2021-07-02 00:24:40 -0400

    set up express back end

---
+++ app.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+const express = require("express")
+const cors = require("cors")
+const bookmarksController = require("./controllers/bookmarksController.js")
+
+// CONFIGURATION
+const app = express()
+
+// MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.send("Welcome to the Bookmarks API!!!")
+})
+
+app.use("/bookmarks", bookmarksController)
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found")
+})
+
+
+// EXPORT
+module.exports = app
---
+++ controllers/bookmarksController.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const bookmarks = express.Router()
+
+bookmarks.get("/", (req, res) => {
+    res.json({
+        status: "ok"
+    })
+})
+
+module.exports = bookmarks;
