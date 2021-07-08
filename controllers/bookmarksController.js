e57f834af6f62f482cb569da98fbb2b317df0f10
Ivan Castillo <total180training@gmail.com>
2021-07-07 23:32:07 -0400

    Added server routes for Show, Create, Delete, Update

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const bookmarks = express.Router()
-const { getAllBookmarks } = require("../queries/bookmarks.js")
+const { getAllBookmarks, getBookmark, createBookmark, deleteBookmark, updateBookmark } = require("../queries/bookmarks.js")
 
 
 // We need to send a request for some data in this endpoint "/"
@@ -10,10 +10,78 @@ const { getAllBookmarks } = require("../queries/bookmarks.js")
             // We do this in a new JS file which will hold this data. So let's create it in the root.
             // queries/bookmarks.js
 
-            
+// Index
 bookmarks.get("/", async (req, res) => {
     const allBookmarks = await getAllBookmarks()
     res.json(allBookmarks)
 })
+
+// Show
+bookmarks.get("/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const bookmark = await getBookmark(id)
+        if (bookmark["id"]) {
+            res.json(bookmark)
+        } else {
+            console.log(`Database Error: ${bookmark}`)
+            throw `There is no bookmark with id: ${id}`
+        }
+    } catch (e) {
+        res.status(404).json({error: "Resource not found", message: e})
+    }
+})
+
+// Create
+bookmarks.post("/", async (req, res) => {
+    try {
+        const bookmark = await createBookmark(req.body)
+        if (bookmark.name) {
+            res.json(bookmark)
+        } else {
+            console.log(`Database error: ${bookmark}`)
+            throw `Error adding ${req.body} to the database`
+        }
+    } catch (e) {
+        console.log(`Error in queries: ${e}`)
+        res.status(404).json({ error: `${e}` })
+    }
+})
+
+// for errors, "relation" means the same thing as "table"
+
+// Delete
+bookmarks.delete("/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const deleted = await deleteBookmark(id)
+        if (deleted.name) {
+            res.status(200).json(deleted)
+        } else {
+            throw "Resource not found"
+        }
+    } catch(e) {
+        console.log(`Error: ${e}`)
+        return e
+    }
+})
+
+// Update
+bookmarks.put("/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const update = await updateBookmark(req.body, id)
+        if (update.id) {   
+            res.json({update})
+        } else {
+            res.status(404).send(`Bookmark with id of ${id} does not exist`)
+        }
+    } catch (e) {
+        console.log(`Error: ${e}`)
+        res.status(404).json({Error: `${e}`})
+    }
+})
+
+
 
 module.exports = bookmarks;
--- queries/bookmarks.js
+++ queries/bookmarks.js
@@ -1,3 +1,4 @@
+const bookmarks = require("../controllers/bookmarksController");
 const db = require("../db/dbConfig");
 
 const getAllBookmarks = async () => {
@@ -11,6 +12,60 @@ const getAllBookmarks = async () => {
     };
 };
 
+const getBookmark = async (id) => {
+    try {
+        // the first argument is a query, the second is the variable referenced in query with $1
+        const oneBookmark = await db.one("SELECT * FROM bookmarks WHERE id=$1", id)
+        if (oneBookmark["id"]) {
+            return oneBookmark
+        } else {
+            throw oneBookmark
+        }
+    } catch (e) {
+        return e
+    }
+}
+
+const createBookmark = async (bookmark) => {
+    try {
+        if (!bookmark.name) {
+            throw "You must specify a value for name"
+        }
+        // first argument is a query, the second is an array
+        const newBookmark = await db.one(
+            "INSERT INTO bookmarks (name, url, is_favorite) VALUES ($1, $2, $3) RETURNING *",
+            [bookmark.name, bookmark.url, bookmark.is_favorite]
+        )
+        return newBookmark
+    } catch (e) {
+        console.log(`Error in queries ${e}`)
+        return `Error in queries ${e}`
+    }
+}
+
+const deleteBookmark = async (id) => {
+    try {
+        // db.one or .any returns a promise, so we need to store it in a variable and await it, then return it
+        const deletedBookmark = await db.one("DELETE FROM bookmarks WHERE id=$1 RETURNING *", id)
+        return deletedBookmark
+    } catch (e) {
+        return e
+    }
+}
+
+const updateBookmark = async (bookmark, id) => {
+    try {
+        const update = await db.one(
+            "UPDATE bookmarks SET name=$1, url=$2, category=$3, is_favorite=$4 WHERE id=$5 RETURNING *",
+            [bookmark.name, bookmark.url, bookmark.category, bookmark.is_favorite, id]
+        )
+        return update
+    } catch (e) {
+        return e
+    }
+}
+
+
 module.exports = {
-    getAllBookmarks
+    getAllBookmarks, getBookmark, createBookmark, deleteBookmark, updateBookmark
 };
